61c6e6b4cce7afd1cde6fe0ea019738bb398edd9
Johncyrebecca684 <johncyrebecca@gmail.com>
2025-06-16 13:25:01 +0530

    day1 all classes

---
+++ Samplejava/src/DAY1/IdentiferDemo.java
@@ -0,0 +1,13 @@
+package DAY1;
+
+public class IdentiferDemo {
+
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("value of the number variable is : "+ $number);
+		
+		String studentName="Aniket";
+		System.out.println("value is : "+ studentName);
+	}
+
+}
---
+++ Samplejava/src/DAY1/studentDemo.java
@@ -0,0 +1,15 @@
+package DAY1;
+
+public class studentDemo {
+
+	public static void main(String[] args) {
+		student st = new student();
+		st.setId(101);
+		st.setName("John");
+		st.setAge(20);
+		
+		System.out.println(st);
+
+	}
+
+}
